b857fdd9910f2d05c7cc4746a162b73632994a75
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2022-12-11 19:34:04 +0800

    Add files via upload

---
+++ menukar nilai variabel.py
@@ -0,0 +1,15 @@
+# Menginput Nilai Variabel
+x = input("Tuliskan Nilai X : ")
+y = input("Tuliskan Nilai Y : ")
+
+print(f"Nilai X : {x}")
+print(f"Nilai Y : {y}")
+
+# Membuat Variabel tukar dan Menukar nilai Variabel lain
+tukar = x
+x = y
+y = tukar
+ 
+#Menampilkan Nilai Variabel Setelah Ditukar
+print(f"Nilai X Setelah Ditukar Adalah: {x}")
+print(f"Nilai Y Setelah Ditukar Adalah: {y}")
